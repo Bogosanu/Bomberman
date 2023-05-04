b739f1dd320947fbe9d3dae6b48f18c89e61c94f
Bogosanu <Bogosanu@users.noreply.github.com>
2023-05-04 17:04:11 +0300

    player-ul se poate misca

--- Game.cpp
+++ Game.cpp
@@ -22,7 +22,10 @@ const Player &Game::getPlayer() const {
 void Game::generatelevel(){
         std::ifstream fin("level.txt");
         int i, x, y, hp, n;
-        fin>>n;
+        fin >> x >> y;
+        if(player.can_be_placed(x,y,m))
+            player.setpos(x,y);
+        fin >> n;
         for(i = 0; i < n; ++i){
             fin >> x >> y;
             this->getMbr().getWallmatrix()[x].getRow()[y].setdetails(x,y,2);
@@ -32,8 +35,6 @@ void Game::generatelevel(){
             this->getMbr().getWallmatrix()[x].getRow()[y].setdetails(x,y,hp);
             m[x][y] = this->getMbr().getWallmatrix()[x].getRow()[y].gethp();
         }
-        if(player.can_be_placed(1,1,m))
-            player.setpos(1,1);
 
 
 
@@ -42,6 +43,8 @@ void Game::generatelevel(){
     void Game::drawlevel() {
        int i, j, br;
         sf::RenderWindow window(sf::VideoMode(950, 650), "BOMBERMAN", sf::Style::Close | sf::Style::Titlebar);
+        std::vector<sf::Sprite> sp_board;
+        sf::Sprite player1;
         sf::Sprite curr_brick;
         sf::Texture hardbrick;
         hardbrick.loadFromFile("Textures/hardblock2.png");
@@ -53,41 +56,14 @@ void Game::generatelevel(){
         objective_hurt.loadFromFile("Textures/objective_hurt.png");
         sf::Texture player_text;
         player_text.loadFromFile("Textures/player.png");
+        player1.setTexture(player_text);
+        player1.setScale(0.5f, 0.5f);
+        player1.setPosition(player.getY()*50, player.getX()*50);
 
 
-        window.setFramerateLimit(15);
+        window.setFramerateLimit(7);
         while (window.isOpen()) {
             sf::Event evnt;
-            for (i = 0; i <= 12 * 50; i += 50) {
-                for (j = 0; j <= 18 * 50; j += 50) {
-                    br = m[i/50][j/50];
-                    if (br == 9){
-                        curr_brick.setTexture(hardbrick);
-                        curr_brick.setScale(0.5f, 0.5f);
-                        curr_brick.setPosition(float(j), float(i));
-                    }
-                    else if (br == 1) {
-                        curr_brick.setTexture(softbrick);
-                        curr_brick.setScale(0.5f, 0.5f);
-                        curr_brick.setPosition(float(j), float(i));
-                    }
-                    else if (br == 2){
-                        curr_brick.setTexture(objective);
-                        curr_brick.setScale(0.5f, 0.5f);
-                        curr_brick.setPosition(float(j), float(i));
-                    }
-                    else if (br == 0 && j == player.getX() && i == player.getY()){
-                        curr_brick.setTexture(objective);
-                        curr_brick.setScale(0.5f, 0.5f);
-                        curr_brick.setPosition(float(j), float(i));
-                    }
-                    window.draw(curr_brick);
-
-                }
-            }
-
-
-
             while (window.pollEvent(evnt)) {
                 switch (evnt.type) {
                     case sf::Event::Closed:
@@ -96,14 +72,72 @@ void Game::generatelevel(){
                     default:
                         continue;
                 }
+            }
+
+            sp_board.clear();
+            window.clear(sf::Color::Black);
+
+            for (i = 0; i <= 12 * 50; i += 50) {
+                for (j = 0; j <= 18 * 50; j += 50) {
+                    br = this->getM()[i/50][j/50];
+                    if (br == 9){
+                        curr_brick.setTexture(hardbrick);
+                        curr_brick.setScale(0.5f, 0.5f);
+                        curr_brick.setPosition(float(j), float(i));
+                        sp_board.push_back(curr_brick);
+                    }
+                    else if (br == 1) {
+                        curr_brick.setTexture(softbrick);
+                        curr_brick.setScale(0.5f, 0.5f);
+                        curr_brick.setPosition(float(j), float(i));
+                        sp_board.push_back(curr_brick);
+                    }
+                    else if (br == 2){
+                        curr_brick.setTexture(objective);
+                        curr_brick.setScale(0.5f, 0.5f);
+                        curr_brick.setPosition(float(j), float(i));
+                        sp_board.push_back(curr_brick);
+                    }
+
+                }
 
             }
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)){
+                sf::Vector2f pos = player1.getPosition();
+                if(m[pos.y/50+1][pos.x/50] == 0)
+                pos.y+=50;
+                player1.setPosition(pos);
+            }
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)){
+                sf::Vector2f pos = player1.getPosition();
+                if(m[pos.y/50-1][pos.x/50] == 0)
+                pos.y-=50;
+                player1.setPosition(pos);
+            }
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)){
+                sf::Vector2f pos = player1.getPosition();
+                if(m[pos.y/50][pos.x/50-1] == 0)
+                pos.x-=50;
+                player1.setPosition(pos);
+            }
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)){
+                sf::Vector2f pos = player1.getPosition();
+                if(m[pos.y/50][pos.x/50+1] == 0)
+                pos.x+=50;
+                player1.setPosition(pos);
+            }
+
+            window.draw(player1);
+            int n = sp_board.size();
+            for(i = 0; i < n; i++)
+                window.draw(sp_board[i]);
             window.display();
+
+
 
         }
 
 
 
     }
-
 
